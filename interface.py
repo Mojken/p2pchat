f22c6ac886596c557ea30c10cdefb54bd3f07972
Mikael Ivarsson <mikaelgillis@gmail.com>
2021-04-04 23:48:16 +0200

    Add interface

--- interface.py
+++ interface.py
@@ -1,11 +1,51 @@
 #!/usr/bin/env python3
-import tty, sys, termios
+import tty, sys, termios, networking
 
 # Save terminal settings
 orig_settings = termios.tcgetattr(sys.stdin)
 
-tty.setcbreak(sys.stdin)
-print(sys.stdin.read(1)[0])
+mode = True
+def toggle_input_mode():
+    global mode
+    if mode:
+        tty.setcbreak(sys.stdin)
+    else:
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, orig_settings)
+    mode = not mode
 
-# Reset terminal settings to original
+def insert():
+    print("insert")
+    None
+
+def connect():
+    print("connect")
+    toggle_input_mode()
+    address = input("IP: ")
+    networking.connect(address)
+    toggle_input_mode()
+
+def stop():
+    print("stop")
+    global running
+    running = False
+
+
+handlers = {
+    'i': insert,
+    'c': connect,
+    'q': stop
+}
+
+
+toggle_input_mode()
+running = True
+while running:
+    try:
+        char = sys.stdin.read(1)[0]
+        if char in handlers:
+            handlers[char]()
+    except:
+       termios.tcsetattr(sys.stdin, termios.TCSADRAIN, orig_settings)
+       exit()
+
 termios.tcsetattr(sys.stdin, termios.TCSADRAIN, orig_settings)
--- networking.py
+++ networking.py
@@ -1,26 +1,33 @@
 #!/usr/bin/env python3
 import time, socket, threading
 
-port = 1337
-outgoing = []
-incoming = []
+incoming_port = 1337
+
+peers = []
 
 class PeerHandler:
-    def __init__(self, socket, queue):
-        self.socket = socket
-        self.queue = queue
+    def __init__(self, socket=None, address=None):
+        if not socket and address:
+            outgoing_port = 65000+random.randint(1, 500)
+            self.socket = socket.socket()
+            self.socket.connect((address, outgoing_port))
+        elif socket:
+            self.socket = socket
+        elif not socket and not address:
+            raise
+        self.outgoing = []
+        self.incoming = []
         self.loop = False
 
     def listener(self):
-        #upload_port_num = 65000+random.randint(1, 500)
         while self.loop:
-            print(self.socket.recv(4096).decode('utf-8'))
+            self.incoming.append(self.socket.recv(4096).decode('utf-8'))
 
     def sender(self):
         while self.loop:
             if self.queue:
                 print("sending")
-                self.send_socket.send(self.queue.pop().encode('utf-8'))
+                self.send_socket.send(self.outgoing.pop().encode('utf-8'))
 
     def start(self):
         sender = threading.Thread(target=self.sender)
@@ -31,9 +38,12 @@ class PeerHandler:
         listener.start()
         #sender.start()
 
+    def disconnect():
+        this.loop = False
+
 def connection_listener():
     soc = socket.socket()
-    soc.bind(('', port))
+    soc.bind(('', incoming_port))
     soc.listen()
 
     loop = True
@@ -41,17 +51,10 @@ def connection_listener():
         (peer, address) = soc.accept()
         peer.settimeout(None)
         print("Connection from {}".format(address))
-        PeerHandler(peer, outgoing).start()
+        peers.append(PeerHandler(peer, outgoing).start())
 
 connection_listener_thread = threading.Thread(target=connection_listener)
 connection_listener_thread.start()
 
-peer_soc = socket.socket()
-peer_soc.connect((input("IP: "), port))
-
-while True:
-    try:
-        peer_soc.send(input("> ").encode('utf-8'))
-    except KeyboardInterrupt:
-        peer_soc.send("Goodbye".encode('utf-8'))
-        raise
+def connect(address):
+    peers.append(PeerHandler(address=address))
